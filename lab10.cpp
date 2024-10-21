de87535b1223cfc891d58140f7dc7ff059fc7537
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2024-10-21 15:36:30 +0700

    Final version of fast mult in stolbik

--- lab10.cpp
+++ lab10.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 #include <string>
 
+
 using namespace std;
+
+int powerOfTen(int n)
+{
+    int result = 1;
+    for (int i = 0; i < n; ++i)
+        result *= 10;
+    return result;
+}
 
 int multiply(int x, int y)
 {
@@ -12,17 +21,18 @@ int multiply(int x, int y)
     cout << x << " " << y << endl;
 
     int length = max(to_string(x).length(), to_string(y).length());
+    int n = length / 2;
 
-    int a = x / pow(10, length/2);
-    int b = x - a * pow(10, length/2);
-    int c = y / pow(10, length/2);
-    int d = y - c * pow(10, length/2);
+    int a = x / powerOfTen(n);
+    int b = x % powerOfTen(n);
+    int c = y / powerOfTen(n);
+    int d = y % powerOfTen(n);
 
     int ac = multiply(a, c);
     int bd = multiply(b, d);
     int abcd = multiply(a + b, c + d) - ac - bd;
 
-    return abcd * pow(10, length/2) + ac * pow(10, length) + bd;
+    return ac * powerOfTen(n * 2) + abcd * powerOfTen(n) + bd;
 }
 
 int main()
